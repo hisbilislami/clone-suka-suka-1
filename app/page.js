c818f687eeb1320b5492254982342907b0b33a93
Nor Izza Acarina <norizzaafcarina63809@gmail.com>
2023-08-26 20:42:52 +0700

    navbar

--- app/page.js
+++ app/page.js
@@ -1,18 +1,14 @@
-"use client"
+"use client";
 
-import React from 'react'
-import Coba from './components/Coba'
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar'
-
-
-
+import React from "react";
+import Coba from "./components/Coba";
+import Navbar from "./components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main>
       {/* <Coba /> */}
-      <BrowserRouter><Coba /></BrowserRouter>
+      <Coba />
     </main>
-  )
+  );
 }
