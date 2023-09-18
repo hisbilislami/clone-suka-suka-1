f2a3f2fb1b535fadd62e49a8c6cce6c0756085ef
Nor Izza Acarina <norizzaafcarina63809@gmail.com>
2023-09-18 19:52:26 +0700

    trusted

--- app/page.js
+++ app/page.js
@@ -1,29 +1,22 @@
-<<<<<<< HEAD
+"use client"
 import React from 'react'
 import Trusted from './components/Trusted'
-=======
-"use client";
-import React from "react";
 import Jumbotron from "./components/Jumbotron";
 import Top_category from "./components/Top_category";
-import Coba from "./components/Coba";
 import FeaturedInstructor from "./components/FeaturedInstructor";
+import Coba from './components/Coba';
 import JoinMentor from "./components/JoinMentor";
->>>>>>> 2bbac00eb17669add7e3b3ed3f85c77c3ecae606
 
 export default function Home() {
   return (
     <main>
-<<<<<<< HEAD
-      <Trusted />
-=======
       <Jumbotron />
       <Top_category />
       <Coba />
       <FeaturedInstructor />
+      <Trusted />
       <JoinMentor/>
       
->>>>>>> 2bbac00eb17669add7e3b3ed3f85c77c3ecae606
     </main>
   );
 }
