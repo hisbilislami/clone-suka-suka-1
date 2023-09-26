eafe63c92ba721a132ee49b48b8f09c70cd8a257
agilcetha <agilcetha@gmail.com>
2023-09-26 11:51:03 +0700

    latesblog

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Top_category from "./components/Top_category";
 import Coba from "./components/Coba";
 import FeaturedInstructor from "./components/FeaturedInstructor";
 import JoinMentor from "./components/JoinMentor";
+import LatesBlog from "./components/LatesBlog";
 
 export default function Home() {
   return (
@@ -14,7 +15,7 @@ export default function Home() {
       <Coba />
       <FeaturedInstructor />
       <JoinMentor/>
-      
+      <LatesBlog/>
     </main>
   );
 }
