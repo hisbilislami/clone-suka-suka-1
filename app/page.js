7d87b58f0b0c85d2f4e75076e5815c09652b76a1
hisbil islami <hisbil.prog@gmail.com>
2023-10-12 21:06:56 +0700

    resolved conflict in page.js

--- app/page.js
+++ app/page.js
@@ -1,21 +1,18 @@
-"use client"
-import React from 'react'
-import Trusted from './components/Trusted'
+"use client";
+import React from "react";
+import Trusted from "./components/Trusted";
 import Jumbotron from "./components/Jumbotron";
 import Top_category from "./components/Top_category";
 import FeaturedInstructor from "./components/FeaturedInstructor";
 import StudentCourse from "./components/StudentCourse";
 import ChekcoutReview from "./components/ChekcoutReview";
-import Coba from './components/Coba';
+import Coba from "./components/Coba";
 import JoinMentor from "./components/JoinMentor";
-import Become from './components/Become';
-import Unaccess from './components/Unaccess';
-import LatestBlog from './components/Latestblog';
-<<<<<<< HEAD
-import MasterSkill from './components/MasterSkill';
-=======
->>>>>>> 2c1571355650ec26c8da7b50d3ffa3ec02b78136
-import Footers from './components/Footers';
+import Become from "./components/Become";
+import Unaccess from "./components/Unaccess";
+import LatestBlog from "./components/Latestblog";
+import MasterSkill from "./components/MasterSkill";
+import Footers from "./components/Footers";
 
 export default function Home() {
   return (
@@ -24,16 +21,15 @@ export default function Home() {
       <Top_category />
       <Coba />
       <FeaturedInstructor />
-      <MasterSkill/>
+      <MasterSkill />
       <ChekcoutReview />
       <Trusted />
-      <JoinMentor/>
+      <JoinMentor />
       <Become />
-      <LatestBlog/>
+      <LatestBlog />
       <StudentCourse />
       <Unaccess />
-      <Footers/>
-      
+      <Footers />
     </main>
   );
 }
